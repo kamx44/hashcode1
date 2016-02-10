9136d336ef34ad8262fe17c65b1b88d8a6a9f9d1
Konrad-i5 <Konrad622@gmail.com>
2016-02-10 22:39:27 +0100

    correctness check
    Elo

--- HashCodeTestProject/HashCodeTestProject/Scanner.h
+++ HashCodeTestProject/HashCodeTestProject/Scanner.h
@@ -16,12 +16,14 @@ public:
 	void scan_input();
 	void scan_table();
 	void print_commands();
-
+	void init_test_table();
+	void table_test();
 private:
 	Table *table;
 	vector<string> commands;
 	vector<int*> line_vertical;
 	vector<int*> line_horizontal;
+	Table *test;
 	int r; int c;
 };
 
